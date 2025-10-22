d024bbfaf859a84ba2eee763b167f53ca003068f
ianrwan <ianwan012088@gmail.com>
2025-10-22 15:55:12 +0800

    add full_adder_structural_description

---
+++ week7/full_adder_structural_description.v
@@ -0,0 +1,11 @@
+module full_adder_structural_description(a, b, cin, sum, cout);
+input a, b, cin;
+output sum, cout;
+
+wire w1, w2, w3
+
+half_adder_structural_description half_adder1(a, b, w1, w2);
+half_adder_structural_description half_adder2(cin, w1, sum, w3);
+
+or or1(cout, w3, w2);
+endmodule
