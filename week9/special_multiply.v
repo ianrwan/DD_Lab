80bf6dbc7825f86d93bec0668deecf2d31814d44
ianrwan <ianwan012088@gmail.com>
2025-12-03 10:15:41 +0800

    update w9 seven display 0 1 reverse

--- week9/special_multiply.v
+++ week9/special_multiply.v
@@ -2,16 +2,16 @@ module special_multiply(input [3:0] in, output reg [6:0] out);
 always@(*)
 begin
     case (in)
-        4'b0000: out = 7'b0000110;
-        4'b0001: out = 7'b1011011;
-        4'b0010: out = 7'b1001111;
-        4'b0011: out = 7'b1111101;
-        4'b0100: out = 7'b1111111;
-        4'b0101: out = 7'b1110111;
-        4'b0110: out = 7'b1111100;
-        4'b0111: out = 7'b1011110;
-        4'b1000: out = 7'b1110001;
-        default: out = 7'b0111111;
+		4'b0000: out = 7'b1111001; // out = 0+1
+		4'b0001: out = 7'b0100100; // out = 1+1
+		4'b0010: out = 7'b0110000; // out = 2+1
+		4'b0011: out = 7'b0000010; // out = 3*2
+		4'b0100: out = 7'b0000000; // out = 4*2
+		4'b0101: out = 7'b0001000; // out = 5*2
+		4'b0110: out = 7'b0000011; // out = 6*2-1
+		4'b0111: out = 7'b0100001; // out = 7*2-1
+		4'b1000: out = 7'b0001110; // out = 8*2-1
+		default: out = 7'b1000000;
     endcase
 end
 endmodule
